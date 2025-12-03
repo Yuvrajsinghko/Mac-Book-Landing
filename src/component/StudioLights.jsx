6792b20ccafb0b1746d4d1b7d1b8406c400aa577
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-03 17:01:34 +0530

    updated studiolights.jsx

--- src/component/StudioLights.jsx
+++ src/component/StudioLights.jsx
@@ -21,12 +21,23 @@ const StudioLights = () => {
           />
         </group>
       </Environment>
-      <spotLight 
-      position={[-2,10,5]}
-      angle={0.15}
-      decay={0}
-      intensity={Math.PI * 0.2}
-      
+      <spotLight
+        position={[-2, 10, 5]}
+        angle={0.15}
+        decay={0}
+        intensity={Math.PI * 0.2}
+      />
+      <spotLight
+        position={[0, -25, 10]}
+        angle={0.15}
+        decay={0}
+        intensity={Math.PI * 0.2}
+      />
+      <spotLight
+        position={[0,15,5]}
+        angle={0.15}
+        decay={0}
+        intensity={Math.PI * 0.2}
       />
     </group>
   );
