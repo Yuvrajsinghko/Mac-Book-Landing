ca298a7538ffbc520c876a18b31e39598ec85e41
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-04 14:53:56 +0530

    completed showcase section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import ProductViewer from "./component/ProductViewer";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import Showcase from "./component/Showcase";
+import Performance from "./component/Performance";
+import Features from "./component/Features";
+import Highlights from "./component/Highlights";
+import Footer from "./component/Footer";
 
 gsap.registerPlugin(ScrollTrigger);
 function App() {
@@ -14,6 +18,10 @@ function App() {
       <Hero />
       <ProductViewer />
       <Showcase/>
+      <Performance />
+      <Features />
+      <Highlights />
+      <Footer />
     </main>
   );
 }
